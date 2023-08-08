a5b21b46db15a10a9c0111efaed4bcf46b9447bd
Abhishek <aby39838@gmail.com>
2023-08-08 15:31:02 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+//dot config
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+// app.use() -----> This function adds a new middleware to the app.
+//This middleware is responsible for parsing the request body and setting the req.body property
+app.use(express.json());
+//This middleware attaches CORS headers to your Express HTTP responses
+// CORS headers allow apps running in the browser to make requests to servers on different domains 
+app.use(cors());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+      .bgBlue.white
+  );
+});
